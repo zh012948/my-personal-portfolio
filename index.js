fc1de141dfde05ee687ee5af390a674633866ed1
Zeeshan Haider <zh012948@gmail.com>
2024-08-18 23:11:34 +0500

    All Done Except Portfolio Projects

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
 
 toggle.addEventListener('click', () => nav.classList.toggle('active'))
+
+
+
+function contact() {
+    window.location.href = "#contact";
+}
